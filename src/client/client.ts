d07bef968a775286f54ec2b72cb6abed588f81aa
aaron777collins <aaron777collins@gmail.com>
2023-04-21 20:19:56 -0400

    added sprites but the animation is still broken

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,87 +1,426 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'stats.js'
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.OrthographicCamera(
+    window.innerWidth / -2,
+    window.innerWidth / 2,
+    window.innerHeight / 2,
+    window.innerHeight / -2,
+    1,
+    1000
+)
+camera.position.z = 10
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const controls = new OrbitControls(camera, renderer.domElement)
-
-const stats = new Stats();
+const stats = new Stats()
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
+    camera.left = window.innerWidth / -2
+    camera.right = window.innerWidth / 2
+    camera.top = window.innerHeight / 2
+    camera.bottom = window.innerHeight / -2
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
 
 function animate() {
-    stats.begin();
-
-    controls.update()
-
+    stats.begin()
+    update()
     render()
-    stats.end();
+    stats.end()
     requestAnimationFrame(animate)
 }
 
+function update() {
+    updateCharacter()
+}
+
 function render() {
+    updateAnimation()
     renderer.render(scene, camera)
 }
 
-function addHelpText() {
-    const helpText = document.createElement('div');
-    helpText.style.position = 'absolute';
-    helpText.style.width = "100%";
-    helpText.style.height = "100%";
-    helpText.style.color = 'white';
-    helpText.innerHTML = 'Use the mouse to rotate the camera';
-    helpText.style.top = 50 + '%';
-    helpText.style.left = 50 + '%';
-    helpText.style.fontSize = 50 + 'px';
-    helpText.style.textAlign = 'center';
-    helpText.style.transform = 'translate(-50%, -50%)';
-    document.body.appendChild(helpText);
+// Your addHelpText function remains the same
 
-    setTimeout(() => {
-        helpText.style.display = 'none';
-    }, 3000);
-
-    helpText.addEventListener('click', () => {
-        helpText.style.display = 'none';
-    });
+// Character movement and flipping logic
+const character = {
+    sprite: null,
+    flipped: false,
+    velocity: new THREE.Vector2(0, 0),
+    speed: 3,
+    speedFrictionRatio: 0.9,
+    textures: [],
+    attacking: false,
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+const enum DIRECTIONS {
+    normal = 'normal',
+    right = 'right',
+    back = 'back',
+}
 
-    // creating water scene
+// frames go from right to left
+const animationData = {
+    idle: {
+        startFrame: 0,
+        endFrame: 5,
+        framesMissing: 0,
+    },
+    idleRight: {
+        startFrame: 6,
+        endFrame: 11,
+        framesMissing: 0,
+    },
+    idleBack: {
+        startFrame: 12,
+        endFrame: 17,
+        framesMissing: 0,
+    },
+    walk: {
+        startFrame: 18,
+        endFrame: 23,
+        framesMissing: 0,
+    },
+    walkRight: {
+        startFrame: 24,
+        endFrame: 29,
+        framesMissing: 0,
+    },
+    walkBack: {
+        startFrame: 30,
+        endFrame: 35,
+        framesMissing: 0,
+    },
+    attack: {
+        startFrame: 36,
+        endFrame: 41,
+        framesMissing: 2,
+    },
+    attackRight: {
+        startFrame: 42,
+        endFrame: 47,
+        framesMissing: 2,
+    },
+    attackBack: {
+        startFrame: 48,
+        endFrame: 53,
+        framesMissing: 2,
+    },
+    die: {
+        startFrame: 54,
+        endFrame: 59,
+        framesMissing: 3,
+    },
+    mostRecentDirection: DIRECTIONS.normal,
+    // Add more animations here
+    firstFlipID: 0, // default value
 
-    // translate camera view the water at a 45 degree angle, offset by 1 unit
-    camera.position.set(0, 5, 5);
-    camera.lookAt(0, 0, 0);
+    currentAnimation: 'idle',
+    previousAnimation: '',
+    animationElapsedTime: 0,
+    animationDuration: 1000, // In milliseconds
+    animationTimeOffset: 0,
+}
 
-    const waterGeometry = new THREE.PlaneGeometry(100, 100, 100, 100);
-    const waterMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true });
-    const water = new THREE.Mesh(waterGeometry, waterMaterial);
-    water.rotation.x = -Math.PI / 2;
+function createTextureAtlasGrid(atlasTexture, tilesHoriz, tilesVert, flipped=false) {
+    const textures: THREE.Texture[] = []
+    for (let i = 0; i < tilesVert; i++) {
+        for (let j = tilesHoriz-1; j >= 0; j--) {
+            const tileTexture = atlasTexture.clone()
+            tileTexture.needsUpdate = true
+            tileTexture.repeat.set(1 / tilesHoriz, 1 / tilesVert)
+            tileTexture.offset.set(j / tilesHoriz, i / tilesVert)
+            textures.push(tileTexture)
+        }
+    }
+    return textures
+}
 
-    scene.add(water);
+function createCharacter(textureAtlas: THREE.Texture, tilesHoriz = 1, tilesVert = 1) {
+    const textures = createTextureAtlasGrid(textureAtlas, tilesHoriz, tilesVert).reverse()
+    const flippedatlas = textureAtlas.clone()
+    const flippedTextures = createTextureAtlasGrid(flippedatlas, tilesHoriz, tilesVert, true).reverse()
+    flippedTextures.forEach((texture) => {
+        // Flip the texture horizontally
+        texture.wrapS = THREE.RepeatWrapping
+        texture.repeat.x = texture.repeat.x * -1
+    })
 
-    // adding help text to the center of the screen that disappears
-    addHelpText();
+    const combinedTextures = textures.concat(flippedTextures)
 
-    // add fps counter in top left
-    stats.showPanel(0);
-    document.body.appendChild(stats.dom);
+    const spriteMaterial = new THREE.SpriteMaterial({
+        map: combinedTextures[0],
+        color: 0xffffff,
+    })
+    spriteMaterial.side = THREE.DoubleSide
+    spriteMaterial.transparent = true
 
+    const sprite = new THREE.Sprite(spriteMaterial)
+    sprite.scale.set(100, 100, 1)
+    scene.add(sprite)
 
+    animationData.firstFlipID = Math.floor(combinedTextures.length / 2)
 
-    animate();
-});
+    return { sprite: sprite, textures: combinedTextures }
+}
+
+function updateAnimation() {
+    if (animationData.previousAnimation !== animationData.currentAnimation) {
+        console.log("Reset")
+        animationData.animationTimeOffset = -stats.domElement.ownerDocument.defaultView.performance.now()
+    }
+
+    animationData.previousAnimation = animationData.currentAnimation
+    const anim = animationData[animationData.currentAnimation]
+    if (!anim) return
+
+    animationData.animationElapsedTime += animationData.animationTimeOffset + stats.domElement.ownerDocument.defaultView.performance.now()
+
+    if (animationData.animationElapsedTime >= animationData.animationDuration) {
+        // regular adds framesMissing to startFrame but flipped does not
+        const realStartFrame = anim.startFrame + anim.framesMissing
+        const realEndFrame = anim.endFrame
+
+        const currentFrame =
+            ((realStartFrame + Math.floor(animationData.animationElapsedTime / animationData.animationDuration)) %
+            (realEndFrame - realStartFrame + 1))
+
+        if (currentFrame > realEndFrame) {
+            animationData.currentAnimation = 'idle'
+            character.attacking = false
+        }
+        console.log(currentFrame)
+        const frameOffsetForFlip = animationData.firstFlipID
+        const flipOffset = character.flipped ? frameOffsetForFlip : 0
+        character.sprite.material.map =
+            character.textures[flipOffset + realStartFrame + currentFrame]
+        character.sprite.material.needsUpdate = true
+        animationData.animationElapsedTime = 0
+    }
+}
+
+const KEY_STATES = {
+    UP: false,
+    DOWN: false,
+    LEFT: false,
+    RIGHT: false,
+    SPACE: false,
+}
+
+function handleKeyDown(event) {
+    switch (event.code) {
+        case 'ArrowLeft':
+            KEY_STATES.LEFT = true
+            KEY_STATES.RIGHT = false
+            break
+        case 'ArrowRight':
+            KEY_STATES.RIGHT = true
+            KEY_STATES.LEFT = false
+            break
+        case 'ArrowUp':
+            KEY_STATES.UP = true
+            KEY_STATES.DOWN = false
+            break
+        case 'ArrowDown':
+            KEY_STATES.DOWN = true
+            KEY_STATES.UP = false
+            break
+        case 'Space':
+            KEY_STATES.SPACE = true
+            break
+    }
+}
+
+function handleKeyUp(event) {
+    switch (event.code) {
+        case 'ArrowLeft':
+            KEY_STATES.LEFT = false
+            break
+        case 'ArrowRight':
+            KEY_STATES.RIGHT = false
+            break
+        case 'ArrowUp':
+            KEY_STATES.UP = false
+            break
+        case 'ArrowDown':
+            KEY_STATES.DOWN = false
+            break
+        case 'Space':
+            KEY_STATES.SPACE = false
+            break
+    }
+}
+
+function isFlipped() {
+    // flipped if left is pressed.
+    // not flipped if right, up or down is pressed.
+    let flipped = character.flipped
+    if (KEY_STATES.LEFT && !KEY_STATES.UP && !KEY_STATES.DOWN) {
+        flipped = true
+    } else if (KEY_STATES.RIGHT) {
+        flipped = false
+    } else if (KEY_STATES.UP) {
+        flipped = false
+    } else if (KEY_STATES.DOWN) {
+        flipped = false
+    }
+
+    return flipped
+}
+
+function updateCharacter() {
+    if (!character.attacking) {
+        character.flipped = isFlipped()
+
+        if (KEY_STATES.LEFT) {
+            character.velocity.x = -character.speed
+            animationData.currentAnimation = 'walkRight'
+            animationData.mostRecentDirection = DIRECTIONS.right
+        } else if (KEY_STATES.RIGHT) {
+            character.velocity.x = character.speed
+            animationData.currentAnimation = 'walkRight'
+            animationData.mostRecentDirection = DIRECTIONS.right
+        }
+        if (KEY_STATES.UP) {
+            character.velocity.y = character.speed
+            animationData.currentAnimation = 'walkBack'
+            animationData.mostRecentDirection = DIRECTIONS.back
+        } else if (KEY_STATES.DOWN) {
+            character.velocity.y = -character.speed
+            animationData.currentAnimation = 'walk'
+            animationData.mostRecentDirection = DIRECTIONS.normal
+        }
+
+        if (KEY_STATES.SPACE && !character.attacking) {
+            character.attacking = true
+            character.velocity.x /= 2
+            character.velocity.y /= 2
+            // timeout for the frames of the attack animation
+            setTimeout(() => {
+                character.attacking = false
+            }, (animationData[animationData.currentAnimation].endFrame - animationData[animationData.currentAnimation].startFrame) * animationData.animationDuration)
+            // attack
+            if (animationData.mostRecentDirection === DIRECTIONS.normal) {
+                animationData.currentAnimation = 'attack'
+            } else if (animationData.mostRecentDirection === DIRECTIONS.back) {
+                animationData.currentAnimation = 'attackBack'
+            } else if (animationData.mostRecentDirection === DIRECTIONS.right) {
+                animationData.currentAnimation = 'attackRight'
+            } else {
+                // should not be reached
+                animationData.currentAnimation = 'attack'
+            }
+        }
+    }
+
+    character.velocity.x *= character.speedFrictionRatio
+    if (Math.abs(character.velocity.x) < 0.1) {
+        character.velocity.x = 0
+    }
+
+    character.velocity.y *= character.speedFrictionRatio
+    if (Math.abs(character.velocity.y) < 0.1) {
+        character.velocity.y = 0
+    }
+
+    // If no key is pressed, return to idle
+    let animating = character.attacking // same as false || character.attacking
+    Object.keys(KEY_STATES).forEach((key) => {
+        if (KEY_STATES[key]) {
+            animating = true
+        }
+    })
+    if (!animating && character.velocity.x === 0 && character.velocity.y === 0) {
+        if (animationData.mostRecentDirection === DIRECTIONS.normal) {
+            animationData.currentAnimation = 'idle'
+        } else if (animationData.mostRecentDirection === DIRECTIONS.back) {
+            animationData.currentAnimation = 'idleBack'
+        } else if (animationData.mostRecentDirection === DIRECTIONS.right) {
+            animationData.currentAnimation = 'idleRight'
+        } else {
+            // should not be reached
+            animationData.currentAnimation = 'idle'
+        }
+    }
+
+    // Update character position
+    character.sprite.position.x += character.velocity.x
+    character.sprite.position.y += character.velocity.y
+}
+
+// Background-related logic
+function createBackground() {
+    const textureLoader = new THREE.TextureLoader()
+    const backgroundTexture = textureLoader.load('../../static/background.jpg')
+    const backgroundMaterial = new THREE.SpriteMaterial({ map: backgroundTexture, color: 0xffffff })
+
+    const background = new THREE.Sprite(backgroundMaterial)
+    background.scale.set(2000, 1000, 1) // Adjust the scale according to your needs
+    background.position.set(0, 0, -5) // Move background behind the character
+
+    scene.add(background)
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    // add stats
+    document.body.appendChild(stats.dom)
+    stats.showPanel(0)
+
+    createBackground() // Add the background to the scene
+
+    const textureLoader = new THREE.TextureLoader()
+    const atlasTexture = textureLoader.load(
+        '../../static/character.png',
+        (texture) => {
+            //callback
+            const characterData = createCharacter(atlasTexture, 6, 10) // Assuming a 6x10 grid
+            character.sprite = characterData.sprite
+            character.textures = characterData.textures
+
+            // Add event listeners for keyboard controls
+            window.addEventListener('keydown', handleKeyDown)
+            window.addEventListener('keyup', handleKeyUp)
+
+            // Your help text and stats code remains the same
+            animate()
+        },
+        (event) => {
+            // onProgress
+            // create loading dom in the middle of the page
+            const loadingDom = document.createElement('div')
+
+            loadingDom.style.position = 'absolute'
+            loadingDom.style.top = '50%'
+            loadingDom.style.left = '50%'
+            loadingDom.style.transform = 'translate(-50%, -50%)'
+
+            loadingDom.innerText = `${Math.round((event.loaded / event.total) * 100)}%`
+
+            document.body.appendChild(loadingDom)
+        },
+        (err) => {
+            // onError
+            console.error('Unexpected error while loading texture: ', err)
+
+            // display error message in the middle of the page
+            const errorDom = document.createElement('div')
+
+            errorDom.style.position = 'absolute'
+
+            errorDom.style.top = '50%'
+            errorDom.style.left = '50%'
+            errorDom.style.transform = 'translate(-50%, -50%)'
+
+            errorDom.innerText = 'Unexpected error while loading texture'
+
+            document.body.appendChild(errorDom)
+        }
+    )
+})
